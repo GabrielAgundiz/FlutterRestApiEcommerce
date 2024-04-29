e92a03d55854d4831526060f1a92b749d74747d8
Americo <agzz2003@gmail.com>
2024-04-29 02:19:38 -0600

    Configuracion de bloc en el arbol de widgets

--- ecommercegallery/lib/main.dart
+++ ecommercegallery/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:ecommercegallery/screens/home.dart';
+import 'package:ecommercegallery/state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,8 +18,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      //estado inicial de la lista es vacia
+      home: BlocProvider(
+        create: (_) => ShoppingCarBloc(shoppingCarState([])),
+        child: const HomePage(),
+      ),
     );
   }
 }
-
